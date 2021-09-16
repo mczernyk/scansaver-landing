e81cfc33c04a912b60e796cd9e6b1993b0f87650
mczernyk <mczernyk@gmail.com>
2021-09-16 19:05:13 -0400

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,14 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { RiQrCodeLine } from 'react-icons/ri';
+
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>There is nothing here, please double check your request.</p>
   </Layout>
 )
 
